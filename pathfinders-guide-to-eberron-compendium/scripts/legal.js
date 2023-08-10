9ec536431a1ed543e94e89e5a055bc92e395655f
Tyler <tyler.nychka@gmail.com>
2023-08-09 20:17:43 -0700

    Updates Pathfinder 2e Module to V11 compatibility

--- pathfinders-guide-to-eberron-compendium/scripts/legal.js
+++ pathfinders-guide-to-eberron-compendium/scripts/legal.js
@@ -1,5 +1,5 @@
 Hooks.once('ready', (canvas) => {
-	const thisVersion = "1.6.4";
+	const thisVersion = "1.7.0";
 	game.settings.register('pathfinders-guide-to-eberron-compendium', 'license-accepted', {
 		name: "License Accepted",
 		scope: "world",
